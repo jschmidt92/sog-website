29170a51eaa739f1c5c75f9f1d72cd7135f30f56
Jacob Schmidt <innovativestudios@outlook.com>
2022-11-26 21:17:44 -0600

    Update 11262022@2110

--- components/content/CardRow.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<section class="grid grid-flow-row grid-cols-1 lg:grid-cols-2 gap-3">
-		<slot />
-	</section>
-</template>
--- content/knowledgebase/about.md
+++
@@ -1,5 +0,0 @@
----
-title: 'Knowledge Base - About'
----
-
-# About
